1df5b22740fc89e7ad0bb3b0e04a8c61af0dbc88
Diego Segura <dieguezz@gmail.com>
2019-03-15 16:52:12 +0100

    remove meta

--- src/modules/home/home.component.js
+++ src/modules/home/home.component.js
@@ -18,11 +18,6 @@ export default function Home() {
           name="google-site-verification"
           content="3qCU_yfpxNkVpXrJ8mKUeyeb2C7FpqnlF6GZSLeHmIA"
         />
-
-        <meta
-          name="google-site-verification"
-          content="3qCU_yfpxNkVpXrJ8mKUeyeb2C7FpqnlF6GZSLeHmIA"
-        />
         <meta
           name="image"
           content="https://sosfelina.z6.web.core.windows.net/static/media/logo.846aa2e2.png"
